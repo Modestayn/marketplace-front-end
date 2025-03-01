fbd658e2e12ab67521543146a7e97a633d23e23e
DarkZeus <serhii.resnyanskyi@gmail.com>
2025-03-01 20:48:51 +0200

    refactor: shadcn init

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import TanStackRouterVite from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     react(),
